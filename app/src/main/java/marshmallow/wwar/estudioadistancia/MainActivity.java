c5923f4e82b3ce4881c241a8a587e35ebc3f04b4
Abraham Luna <abraham11999@hotmail.com>
2020-12-07 21:16:26 -0700

    Commit Inicial

---
+++ app/src/main/java/marshmallow/wwar/estudioadistancia/MainActivity.java
@@ -0,0 +1,90 @@
+package marshmallow.wwar.estudioadistancia;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.material.navigation.NavigationView;
+
+import marshmallow.wwar.estudioadistancia.fragments.ClasesFragment;
+import marshmallow.wwar.estudioadistancia.fragments.CreditosFragment;
+import marshmallow.wwar.estudioadistancia.fragments.LeidosFragment;
+import marshmallow.wwar.estudioadistancia.fragments.PendienteFragment;
+
+public class MainActivity extends AppCompatActivity implements
+        View.OnClickListener,
+        NavigationView.OnNavigationItemSelectedListener {
+
+    private ImageView toggleDrawer;
+    private TextView titulo;
+    private DrawerLayout drawerLayout;
+    private NavigationView navview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initComponents();
+    }
+
+    private void initComponents() {
+        toggleDrawer = findViewById(R.id.toggleDrawer);
+        drawerLayout = findViewById(R.id.drawerLayout);
+        titulo = findViewById(R.id.titulo);
+        navview = findViewById(R.id.nav_view);
+
+        toggleDrawer.setOnClickListener(this);
+        navview.setNavigationItemSelectedListener(this);
+
+        titulo.setText("Tareas Pendientes");
+        changeFragment(new PendienteFragment());
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.toggleDrawer:
+                drawerLayout.openDrawer(GravityCompat.START);
+                break;
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.itemPendiente:
+                changeFragment(new PendienteFragment());
+                titulo.setText("Tareas Pendientes");
+                break;
+            case R.id.itemClases:
+                changeFragment(new ClasesFragment());
+                titulo.setText("Clases");
+                break;
+            case R.id.itemLeidos:
+                changeFragment(new LeidosFragment());
+                titulo.setText("Leídos");
+                break;
+            case R.id.itemCreditos:
+                changeFragment(new CreditosFragment());
+                titulo.setText("Créditos");
+                break;
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    private void changeFragment(Fragment fragment) {
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.fmf, fragment);
+        ft.commit();
+    }
+}
---
+++ app/src/main/java/marshmallow/wwar/estudioadistancia/fragments/LeidosFragment.java
@@ -0,0 +1,28 @@
+package marshmallow.wwar.estudioadistancia.fragments;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import marshmallow.wwar.estudioadistancia.R;
+
+public class LeidosFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_leidos, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
